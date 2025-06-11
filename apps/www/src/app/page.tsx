367254341762638d4fb0e7a81483d1dbd702c1ec
ankit <sahuankit.murhu@gmail.com>
2025-06-11 22:11:04 +0530

    feat(client): chat area

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,7 +1,9 @@
+import ChatArea from "@/components/ChatArea";
+
 export default function Home() {
   return (
-    <div>
-      
+    <div className="inset-0 absolute h-screen w-full flex flex-col items-center justify-center ">
+      <ChatArea />
     </div>
   );
 }
