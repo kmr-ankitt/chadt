8c4226a2069075a9bb5373479346bc227471cbc0
ankit <sahuankit.murhu@gmail.com>
2025-06-13 08:37:01 +0530

    feat(server): message type declaration

--- apps/server/src/socket/chatSocket.ts
+++ apps/server/src/socket/chatSocket.ts
@@ -1,8 +1,9 @@
 import type { Server, Socket } from "socket.io";
+import type { ChatMessage } from "../types/messageType";
 
 export default function handleChat(socket: Socket, io: Server) {
-  socket.on("message", (message) => {
+  socket.on("message", (message: ChatMessage) => {
     console.log(`Received message : `, message);
     socket.broadcast.emit("message", message);
-  })
+  });
 }
---
+++ apps/server/src/types/messageType.ts
@@ -0,0 +1,7 @@
+export type ChatMessage = {
+  sender: string;
+  senderName: string;
+  senderProfilePicture: string;
+  message: string;
+  timestamp: string;
+};
